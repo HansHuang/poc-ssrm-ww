96ac3ab1c9e290af48aec7aad66becd9573a578c
HansHuang <hys0550@sina.com>
2025-04-18 22:38:37 -0400

    cache data in IndexedDB

--- ui/js/storeSvc.js
+++ ui/js/storeSvc.js
@@ -6,7 +6,6 @@ const getStoreSvc = (options) => () => {
 
     return {
         load: async () => {
-            console.log('Start read data IndexedDB');
             const [data] = await fetch('/api/rows?_page=1&_size=1').then(r => r.json()),
                 colDef = Object.keys(data).reduce((res, key) => {
                     res[key] = { datetype: typeof data[key] }
@@ -14,20 +13,30 @@ const getStoreSvc = (options) => () => {
                 }, {});
             Object.assign(colDef.id, { primaryKey: true, autoIncrement: false })
 
-            const isExisted = await connection.initDb({ name: "mainDB", tables: [{ name: "mainTable", columns: colDef }] });
-            console.log('Database initialized:', isExisted);
+            await connection.initDb({ name: "mainDB", tables: [{ name: "mainTable", columns: colDef }] });
             const mainData = await connection.select({
                 from: 'mainTable'
             }).then(x => {
-                console.log('Data from IndexedDB:', x);
+                console.log('Data from IndexedDB:', x.length);
                 return x;
             }).catch(error => {
                 console.error('Error reading data from IndexedDB:', error);
                 return [];
             });
-            console.log('Main data:', mainData);
             return mainData
-        }
+        },
+        upsert: async (rows) => {
+            const result = await connection.insert({
+                into: 'mainTable',
+                upsert: true,
+                values: rows
+            });
+            console.log('Data inserted into IndexedDB:', result);
+        },
+        clear: async () => {
+            await connection.clear('mainTable');
+            console.log('data cleared successfully');
+        },
     }
 
 }
--- ui/js/worker.js
+++ ui/js/worker.js
@@ -1,31 +1,25 @@
 (async () => {
 
-    // importScripts(`/app/js/sqlite3.js`);
-    // sqlite3InitModule().then(sqlite3 => sqlite3.initWorker1API());
-    // sqlite3.initWorker1API = () => {
-    //     console.log('sqlite3 worker1 api initialized');
-    //     postMessage({ type: 'sqlite3-api', result: 'worker1-ready' });
-    // };
-    // sqlite3.initWorker1API();
-
-    
     importScripts('/app/js/dataSvc.js');
     importScripts('/app/js/storeSvc.js');
 
     const dataCache = [],
         storeSvc = getStoreSvc({
-            
+
         })(),
         dataSvc = getDataSvc({
-            targetSize: 1 * 1000,
+            targetSize: 100 * 1000,
             JsStore,
             onInitLoad: (rows) => {
                 dataCache.splice(0, dataCache.length, ...rows);
                 postMessage({ type: 'fullData', payload: dataCache });
+                storeSvc.clear();
+                storeSvc.upsert(rows);
             },
             onNextLoad: (rows) => {
                 dataCache.push(...rows);
                 postMessage({ type: 'transaction', payload: { add: rows } });
+                storeSvc.upsert(rows);
             },
             onRealtime: (rows) => {
                 const transaction = { add: [], update: [] };
@@ -40,36 +34,19 @@
                     }
                 });
                 postMessage({ type: 'transaction', payload: transaction });
+                storeSvc.upsert(rows);
             }
         })();
-    await dataSvc.load();
+
+    const cachedData = await storeSvc.load()
+    if (Array.isArray(cachedData) && cachedData.length) {
+        dataCache.splice(0, dataCache.length, ...cachedData);
+        postMessage({ type: 'fullData', payload: dataCache });
+    } else {
+        console.log('No data in IndexedDB, starting to load from server');
+        await dataSvc.load();
+    }
+
     // dataSvc.listen();
-    
-    console.log('Data service initialized');
-    await storeSvc.load()
-
-
-
-    // const dbRequest = indexedDB.open('MainDB', 1);
-    // dbRequest.onerror = event => console.error('Error opening IndexedDB:', event);
-    // dbRequest.onsuccess = function (event) {
-    //     const db = event.target.result;
-    //     if (!db.objectStoreNames.contains('MainStore')) {
-    //         const store = db.createObjectStore('MainStore', { keyPath: 'id' });
-    //         store.createIndex('id', 'id', { unique: true });
-    //         console.log('Object store created');
-    //         store.transaction.oncomplete = function () {
-    //             const table = db.transaction('MainStore', 'readwrite').objectStore('MainStore');
-    //             dataCache.forEach(row => table.add(row));
-    //             table.oncomplete = function () {
-    //                 console.log('All data written to IndexedDB');
-    //             };
-    //             table.onerror = function (event) {
-    //                 console.error('Error writing to IndexedDB:', event);
-    //             };
-    //         };
-    //     }         
-    // }
-
 
 })()
